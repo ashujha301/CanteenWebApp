99e256698e9bc94d326ed5c2a3c34d55f66a05af
Ayush <ashujha301@gmail.com>
2023-01-05 04:36:24 +0530

    user controller and models for otp

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const userRouter = require('../server/Routers/userRouter');
+
 
 app.use(express.json());
+
+app.use('/api/user', userRouter);
 
 module.exports = app
