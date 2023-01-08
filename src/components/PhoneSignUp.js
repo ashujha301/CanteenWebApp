f407dd4f3562ce53ca85868662adc15bc4560af4
avishah-inc <avi.shah@increscotech.com>
2023-01-08 18:23:53 +0530

    login

--- src/components/PhoneSignUp.js
+++ src/components/PhoneSignUp.js
@@ -5,6 +5,8 @@ import { Button } from "react-bootstrap";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { useUserAuth } from "../context/UserAuthContext";
+import { Box } from "theme-ui";
+import Navbar from "./Navbar";
 
 const PhoneSignUp = () => {
   const [error, setError] = useState("");
@@ -44,49 +46,93 @@ const PhoneSignUp = () => {
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Firebase Phone Auth</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={getOtp} style={{ display: !flag ? "block" : "none" }}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <PhoneInput
-              defaultCountry="IN"
-              value={number}
-              onChange={setNumber}
-              placeholder="Enter Phone Number"
-            />
-            <div id="recaptcha-container"></div>
-          </Form.Group>
-          <div className="button-right">
-            <Link to="/">
-              <Button variant="secondary">Cancel</Button>
-            </Link>
-            &nbsp;
-            <Button type="submit" variant="primary">
-              Send Otp
-            </Button>
-          </div>
-        </Form>
-
-        <Form onSubmit={verifyOtp} style={{ display: flag ? "block" : "none" }}>
-          <Form.Group className="mb-3" controlId="formBasicOtp">
-            <Form.Control
-              type="otp"
-              placeholder="Enter OTP"
-              onChange={(e) => setOtp(e.target.value)}
-            />
-          </Form.Group>
-          <div className="button-right">
-            <Link to="/">
-              <Button variant="secondary">Cancel</Button>
-            </Link>
-            &nbsp;
-            <Button type="submit" variant="primary">
-              Verify
-            </Button>
-          </div>
-        </Form>
-      </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "blue",
+          height: "100vh",
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        <Navbar label="LOGIN" />
+        <Box
+          sx={{
+            alignSelf: "center",
+            justifyContent: "center",
+            width: ["80%", "60%", "50%", "33%"],
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img src="../Canteenlogo.png" alt="Canteen Logo" />
+          <h2 style={{ color: "white", paddingBottom: 20 }}>Sign In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={getOtp} style={{ display: !flag ? "block" : "none" }}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <PhoneInput
+                defaultCountry="IN"
+                value={number}
+                onChange={setNumber}
+                placeholder="Enter Phone Number"
+                countries={["IN"]}
+                addInternationalOption={false}
+              />
+              <div id="recaptcha-container"></div>
+            </Form.Group>
+            <div className="button-right">
+              <Link to="/">
+                <Button
+                  variant="secondary"
+                  style={{ height: 50, borderWidth: 1, borderColor: "white" }}
+                >
+                  Cancel
+                </Button>
+              </Link>
+              &nbsp;
+              <Button
+                type="submit"
+                variant="success"
+                style={{ height: 50, fontWeight: "600", marginLeft: 5 }}
+              >
+                Send Otp
+              </Button>
+            </div>
+          </Form>
+          <Form
+            onSubmit={verifyOtp}
+            style={{ display: flag ? "block" : "none" }}
+          >
+            <Form.Group className="mb-3" controlId="formBasicOtp">
+              <Form.Control
+                type="otp"
+                placeholder="Enter OTP"
+                onChange={(e) => setOtp(e.target.value)}
+              />
+            </Form.Group>
+            <div className="button-right">
+              <Link to="/">
+                <Button
+                  variant="secondary"
+                  style={{ height: 50, borderWidth: 1, borderColor: "white" }}
+                >
+                  Cancel
+                </Button>
+              </Link>
+              &nbsp;
+              <Button
+                type="submit"
+                variant="success"
+                style={{ height: 50, fontWeight: "600", marginLeft: 5 }}
+              >
+                Verify
+              </Button>
+            </div>
+          </Form>
+        </Box>
+      </Box>
     </>
   );
 };
