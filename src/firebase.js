510b70ecda098bf3691c09f247ed19149490e067
Ayush Jha <ashujha301@gmail.com>
2023-02-18 23:45:44 +0530

    feat:firebase sub_over

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 import "firebase/compat/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1AzIkkca6Byc8W2L5eNfZYt5wKCeUz0M",
-  authDomain: "canteenwebapp-48222.firebaseapp.com",
-  projectId: "canteenwebapp-48222",
-  storageBucket: "canteenwebapp-48222.appspot.com",
+  // apiKey: "AIzaSyA1AzIkkca6Byc8W2L5eNfZYt5wKCeUz0M",
+  // authDomain: "canteenwebapp-48222.firebaseapp.com",
+  // projectId: "canteenwebapp-48222",
+  // storageBucket: "canteenwebapp-48222.appspot.com",
   // messagingSenderId: "4763667233",
   // appId: "1:4763667233:web:9f1769290e45f3accc4c60",
   // measurementId: "G-2MHCV8JT59"
