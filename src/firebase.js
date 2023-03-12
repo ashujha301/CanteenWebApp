363d894060063c6e916df763c0ff82bd0f528375
Ayush Jha <ashujha301@gmail.com>
2023-03-12 11:50:18 +0530

    final

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import "firebase/compat/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQb4Qpqv8IuwVfogZMFstGzraZMm-4L3I",
-  authDomain: "canteenwebapp-f706b.firebaseapp.com",
-  projectId: "canteenwebapp-f706b",
-  storageBucket: "canteenwebapp-f706b.appspot.com",
-  messagingSenderId: "992348472642",
-  appId: "1:992348472642:web:a136609eb456e44ce5f4db",
-  measurementId: "G-FVSPD6N5V6"
+  // apiKey: "AIzaSyBQb4Qpqv8IuwVfogZMFstGzraZMm-4L3I",
+  // authDomain: "canteenwebapp-f706b.firebaseapp.com",
+  // projectId: "canteenwebapp-f706b",
+  // storageBucket: "canteenwebapp-f706b.appspot.com",
+  // messagingSenderId: "992348472642",
+  // appId: "1:992348472642:web:a136609eb456e44ce5f4db",
+  // measurementId: "G-FVSPD6N5V6"
 };
 
 // Initialize Firebase
